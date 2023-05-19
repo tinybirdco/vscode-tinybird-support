12330894deb75d6bfcb32c5072ee1a01da3b018f
Raquel Yuste <raquel@tinybird.co>
2023-05-19 17:19:09 +0200

    added play button to actions toolbar

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
-import * as vscode from 'vscode'
-import * as cp from 'child_process'
-import * as path from 'path'
+import * as vscode from "vscode";
+import * as cp from "child_process";
+import * as path from "path";
 
-const infoSql = vscode.window.createOutputChannel('Tinybird SQL', 'sql')
+const infoSql = vscode.window.createOutputChannel("Tinybird SQL", "sql");
 
 /**
  * Retrieves a setting.
@@ -13,10 +13,10 @@ const infoSql = vscode.window.createOutputChannel('Tinybird SQL', 'sql')
  */
 function getConfigValue<T>(key: string, $default: T): string | T {
   try {
-    const config = vscode.workspace.getConfiguration().get('tinybird')
-    return config?.[key as keyof typeof config] || $default
+    const config = vscode.workspace.getConfiguration().get("tinybird");
+    return config?.[key as keyof typeof config] || $default;
   } catch {
-    return $default
+    return $default;
   }
 }
 
@@ -26,13 +26,13 @@ function getConfigValue<T>(key: string, $default: T): string | T {
  * @returns the venv activation command or `true`.
  */
 function getVenvCommand() {
-  let venv = getConfigValue('venv', null)
+  let venv = getConfigValue("venv", null);
   if (venv) {
-    let activate = getConfigValue('venvActivate', 'bin/activate')
-    return `. "${venv}/${activate}"`
+    let activate = getConfigValue("venvActivate", "bin/activate");
+    return `. "${venv}/${activate}"`;
   }
 
-  return 'true'
+  return "true";
 }
 
 /**
@@ -42,164 +42,166 @@ function getVenvCommand() {
  * @returns with the ASCII table.
  */
 function jsonToTable(json: {
-  meta: { name: string; type: string }[]
-  data: Record<string, any>[]
+  meta: { name: string; type: string }[];
+  data: Record<string, any>[];
 }) {
-  const PAD = 2
+  const PAD = 2;
 
   let columns_by_index = [] as {
-    index: number
-    length: number
-    meta: { name: string; type: string }
-  }[]
-  let columns_by_name = {} as Record<string, (typeof columns_by_index)[number]>
-  let index = 0
+    index: number;
+    length: number;
+    meta: { name: string; type: string };
+  }[];
+  let columns_by_name = {} as Record<string, (typeof columns_by_index)[number]>;
+  let index = 0;
   for (let m in json.meta) {
     let meta = json.meta[m],
-      name = meta.name
+      name = meta.name;
 
     columns_by_index[index] = {
       index: index,
       length: name.length + PAD,
-      meta
-    }
-    columns_by_name[name] = columns_by_index[index]
-    index++
+      meta: meta,
+    };
+    columns_by_name[name] = columns_by_index[index];
+    index++;
   }
 
-  let rows = [] as any[]
+  let rows = [] as any[];
 
-  json.data.forEach(data => {
-    let row = []
+  json.data.forEach((data) => {
+    let row = [];
 
     for (let k in data) {
-      if (!data.hasOwnProperty(k)) continue
+      if (!data.hasOwnProperty(k)) continue;
 
-      let meta = columns_by_name[k]
+      let meta = columns_by_name[k];
 
-      let v
+      let v;
       try {
-        v = data[k].toString()
+        v = data[k].toString();
       } catch {
-        v = ''
+        v = "";
       }
 
-      row[meta.index] = v
-      if (v.length + PAD > meta.length) meta.length = v.length + PAD
+      row[meta.index] = v;
+      if (v.length + PAD > meta.length) meta.length = v.length + PAD;
     }
 
-    rows.push(row)
-  })
+    rows.push(row);
+  });
 
   function padValue(index: number, value: string) {
-    let length = columns_by_index[index].length
-    if (value === undefined || value.length >= length) return value
+    let length = columns_by_index[index].length;
+    if (value === undefined || value.length >= length) return value;
 
-    let type = columns_by_index[index].meta.type.toLowerCase()
-    if (type == 'string' || type == 'date' || type == 'datetime')
-      return ` ${value}`.padEnd(length, ' ')
-    else return `${value} `.padStart(length, ' ')
+    let type = columns_by_index[index].meta.type.toLowerCase();
+    if (type === "string" || type === "date" || type === "datetime")
+      return ` ${value}`.padEnd(length, " ");
+    else return `${value} `.padStart(length, " ");
   }
 
   function makeRow(values: string[], mid: string, left: string, right: string) {
-    let cols = values.map((col, i) => padValue(i, col))
-    return left + cols.join(mid) + right
+    let cols = values.map((col, i) => padValue(i, col));
+    return left + cols.join(mid) + right;
   }
 
   function makeSeparator(mid: string, left: string, right: string) {
-    let values = columns_by_index.map(c => '━'.repeat(c.length))
-    return makeRow(values, mid, left, right)
+    let values = columns_by_index.map((c) => "━".repeat(c.length));
+    return makeRow(values, mid, left, right);
   }
 
-  let output = []
-  let separator = makeSeparator('╋', '┣', '┫')
+  let output = [];
+  let separator = makeSeparator("╋", "┣", "┫");
 
-  output.push(makeSeparator('┳', '┏', '┓'))
+  output.push(makeSeparator("┳", "┏", "┓"));
   output.push(
     makeRow(
-      columns_by_index.map(c => c.meta.name),
-      '┃',
-      '┃',
-      '┃'
+      columns_by_index.map((c) => c.meta.name),
+      "┃",
+      "┃",
+      "┃"
     )
-  )
-  output.push(separator)
-  rows.forEach(row => {
-    output.push(makeRow(row, '┃', '┃', '┃'))
-    output.push(separator)
-  })
-  output.pop()
-  output.push(makeSeparator('┻', '┗', '┛'))
+  );
+  output.push(separator);
+  rows.forEach((row) => {
+    output.push(makeRow(row, "┃", "┃", "┃"));
+    output.push(separator);
+  });
+  output.pop();
+  output.push(makeSeparator("┻", "┗", "┛"));
 
-  return output.join('\n')
+  return output.join("\n");
 }
 
 function getPipeName(baseFileName: string) {
-  let fileName = baseFileName.split('/').slice(-1)
-  let pipeName = undefined
-  if (fileName && fileName.length && fileName[0].endsWith('.pipe')) {
-    pipeName = fileName[0].split('.pipe')[0]
+  let fileName = baseFileName.split("/").slice(-1);
+  let pipeName = undefined;
+  if (fileName && fileName.length && fileName[0].endsWith(".pipe")) {
+    pipeName = fileName[0].split(".pipe")[0];
   }
-  return pipeName
+  return pipeName;
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  const sqlCommand = vscode.commands.registerCommand('tinybird.sql', () => {
-    let editor = vscode.window.activeTextEditor
-    if (!editor) return
+  const sqlCommand = vscode.commands.registerCommand("tinybird.sql", () => {
+    let editor = vscode.window.activeTextEditor;
+    if (!editor) return;
 
-    let pipeName = getPipeName(editor.document.fileName)
-    let query = editor.document.getText(editor.selection)
-    if (!query) return
+    let pipeName = getPipeName(editor.document.fileName);
 
-    let dataProjectSubdir = getConfigValue('dataProjectSubdir', '')
+    let query =
+      editor.document.getText(editor.selection) || editor.document.getText();
+    if (!query) return;
 
-    let workspacePath = vscode.workspace.workspaceFolders?.[0].uri.path || ''
-    let dataProjectPath = path.join(workspacePath, dataProjectSubdir)
+    let dataProjectSubdir = getConfigValue("dataProjectSubdir", "");
 
-    let commands
-    if (query.trim().startsWith('NODE')) {
-      let nodeName = query.trim().split('NODE').slice(-1)[0].trim()
+    let workspacePath = vscode.workspace.workspaceFolders?.[0].uri.path || "";
+    let dataProjectPath = path.join(workspacePath, dataProjectSubdir);
+
+    let commands;
+    if (query.trim().startsWith("NODE")) {
+      let nodeName = query.trim().split("NODE").slice(-1)[0].trim();
       commands = [
         `cd "${dataProjectPath}"`,
         getVenvCommand(),
-        `tb --no-version-warning sql --stats --pipe ${editor.document.fileName} --node ${nodeName} --format json`
-      ]
-      infoSql.appendLine(`NODE ${nodeName}\n`)
+        `tb --no-version-warning sql --stats --pipe ${editor.document.fileName} --node ${nodeName} --format json`,
+      ];
+      infoSql.appendLine(`NODE ${nodeName}\n`);
     } else {
-      let pipeline = ''
+      let pipeline = "";
       if (pipeName !== undefined) {
-        pipeline = `--pipeline ${pipeName}`
+        pipeline = `--pipeline ${pipeName}`;
       }
 
       commands = [
         `cd "${dataProjectPath}"`,
         getVenvCommand(),
-        `tb --no-version-warning sql "${query}" ${pipeline} --stats --format json`
-      ]
+        `tb --no-version-warning sql "${query}" ${pipeline} --stats --format json`,
+      ];
 
-      infoSql.appendLine(`QUERY >\n${query}\n`)
+      infoSql.appendLine(`QUERY >\n${query}\n`);
     }
 
-    let command = commands.join(' && ')
+    let command = commands.join(" && ");
     cp.exec(command, async (err, stdout, stderr) => {
-      if (err) infoSql.appendLine(`ERROR >\n${err}\n${stderr}`)
+      if (err) infoSql.appendLine(`ERROR >\n${err}\n${stderr}`);
       else {
         try {
-          let stats = stdout.split('\n').slice(0, 3).join('\n')
-          let jsonData = stdout.split('\n').slice(3).join('')
-          let data = JSON.parse(jsonData)
-          infoSql.appendLine(`DATA >\n${jsonToTable(data)}\n`)
-          infoSql.appendLine(`STATS >\n${stats}`)
+          let stats = stdout.split("\n").slice(0, 3).join("\n");
+          let jsonData = stdout.split("\n").slice(3).join("");
+          let data = JSON.parse(jsonData);
+          infoSql.appendLine(`DATA >\n${jsonToTable(data)}\n`);
+          infoSql.appendLine(`STATS >\n${stats}`);
         } catch (ex) {
-          infoSql.appendLine(`MESSAGE >\n${stdout}`)
+          infoSql.appendLine(`MESSAGE >\n${stdout}`);
         }
       }
 
-      infoSql.appendLine('')
-      await infoSql.show(true)
-    })
-  })
+      infoSql.appendLine("");
+      await infoSql.show(true);
+    });
+  });
 
-  context.subscriptions.push(sqlCommand)
+  context.subscriptions.push(sqlCommand);
 }
